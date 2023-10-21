86dc86d9846ada527d23b51e380dd44f53def92e
baraka caleb <65470823+pimby@users.noreply.github.com>
2023-10-21 17:34:21 +0300

    addded some code

---
+++ products.js
@@ -0,0 +1,28 @@
+// global input
+import "./javaScript/tooltips.js";
+// specific import
+import { getElement, setLocalStorage } from "./javaScript/utils.js";
+import { getData } from "./javaScript/getData.js";
+import { displayProducts } from "./javaScript/displayProducts.js";
+import { searchFilter } from "./javaScript/searchFilter.js";
+import { categoriesFilter } from "./javaScript/categories.js";
+import { priceFilter } from "./javaScript/priceFilter.js";
+import { CartUnit } from "./javaScript/cart/addToCart.js";
+
+// select item
+const url = "https://fakestoreapi.com/products";
+const products = getElement(".product-content");
+
+window.addEventListener("DOMContentLoaded", async function () {
+  const data = await getData(url).finally(() => {
+    const loader = getElement(".products-loader");
+    loader.classList.add("hide");
+  } );
+  setLocalStorage("store", data);
+  displayProducts(data, products);
+
+  searchFilter(data);
+  categoriesFilter(data);
+  priceFilter(data);
+  CartUnit();
+});
